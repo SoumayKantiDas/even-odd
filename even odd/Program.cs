337a0d77aeed020478b2522c72690f628daa57d4
Soumay kanti Das <anikds2859@outlook.com>
2021-06-08 22:34:44 +0600

    Add project files.

---
+++ even odd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace even_odd
+{
+    class Program
+    {
+    
+
+        
+        static void Main(string[] args)
+        {
+        string val;
+        int res;
+        val = Console.ReadLine();
+        res = Convert.ToInt32(val);
+        bool oddOreven = res % 2 == 0;
+        if(oddOreven)
+        {
+            Console.WriteLine("{0} is even", res);
+        }
+            else
+            {
+                Console.WriteLine("{0} is odd", res);
+            }
+        }
+    }
+}
